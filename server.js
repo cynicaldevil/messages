015e87d43695a2ba929197d03d0d3908c9ec44cf
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2017-01-16 04:05:47 +0530

    Passport oauth basic setup

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+var fs = require('fs');
+
+// express setup
 var app = express();
-
 app.set('view engine', 'ejs');
-
 app.use('/styles', express.static('styles'));
+
+// passport setup
+var oauth_data = JSON.parse(fs.readFileSync('./oauth_client_data.json')).web;
+console.log(oauth_data);
+passport.use(new GoogleStrategy({
+    consumerKey: oauth_data.client_id,
+    consumerSecret: oauth_data.client_secret,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
+  }
+));
 
 app.get('/', function (req, res) {
   res.render('index');
