9d70b34d404c1a8dc6c36d085cc07a986bdd645b
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2017-01-16 04:15:14 +0530

    Upgraded to OAuth 2.0 and added callbacks

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var fs = require('fs');
 
 // express setup
@@ -10,18 +10,28 @@ app.use('/styles', express.static('styles'));
 
 // passport setup
 var oauth_data = JSON.parse(fs.readFileSync('./oauth_client_data.json')).web;
-console.log(oauth_data);
 passport.use(new GoogleStrategy({
-    consumerKey: oauth_data.client_id,
-    consumerSecret: oauth_data.client_secret,
-    callbackURL: "http://www.example.com/auth/google/callback"
+    clientID: oauth_data.client_id,
+    clientSecret: oauth_data.client_secret,
+    callbackURL: "https://localhost:8080/oauth2callback"
   },
-  function(token, tokenSecret, profile, done) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return done(err, user);
-      });
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
   }
 ));
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+        function(req, res) {
+            res.redirect('/');
+        }
+);
 
 app.get('/', function (req, res) {
   res.render('index');
